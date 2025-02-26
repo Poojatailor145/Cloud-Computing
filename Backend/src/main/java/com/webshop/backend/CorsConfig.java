ee92c3cc7092824353bd4101c0b8011612f22346
Pooja Manoj Tailor <poojatailor145@gmail.com>
2025-02-27 00:00:16 +0100

    Filter Products and Integrating API with Frontend

--- Backend/src/main/java/com/webshop/backend/CorsConfig.java
+++ Backend/src/main/java/com/webshop/backend/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:63342") // Adjust as needed
+                        .allowedOrigins("http://127.0.0.1:5500") // Adjust as needed
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
